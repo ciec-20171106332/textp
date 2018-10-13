bd8e7e4e22553a33d389a84c211978cfbbe1e660
ciecS20171106331 <565738655@qq.com>
2018-10-13 09:51:10 +0800

    Initial Commit

---
+++ textp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  textp
+//
+//  Created by s20171106332 on 2018/10/13.
+//  Copyright © 2018年 s20171106332. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
